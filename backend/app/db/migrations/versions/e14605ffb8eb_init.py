b1d48d8babdbb7a58abccb871b62abf73b974f8c
JuliaMucha <julia.mucha257@gmail.com>
2026-05-22 23:42:24 +0200

    style: fix ruff linting errors

--- backend/app/db/migrations/versions/e14605ffb8eb_init.py
+++ backend/app/db/migrations/versions/e14605ffb8eb_init.py
@@ -7,10 +7,6 @@ Create Date: 2026-05-18 20:57:08.752063
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
 revision: str = 'e14605ffb8eb'
 down_revision: Union[str, Sequence[str], None] = None
